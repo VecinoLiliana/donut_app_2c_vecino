603f16e7a046b9971cb965eedaae89b766bc7afd
VecinoLiliana <vecinocollililianamarisol@gmail.com>
2025-02-12 19:03:17 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,16 +12,40 @@ class _HomePageState extends State <HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        //icono izquierdo
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800]
-          ), //icono derecho
-        actions: [
-          Icon(Icons.person)
-        ],
-      )
-    );
+          backgroundColor: Colors.transparent,
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          //Ícono derecho
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text("I want to ", style: TextStyle(fontSize: 32)),
+                  Text("Eat",
+                      style: TextStyle(
+                          //Tamaño de letra
+                          fontSize: 32,
+                          //Negritas
+                          fontWeight: FontWeight.bold,
+                          //Subrayado
+                          decoration: TextDecoration.underline))
+                ],
+              ),
+            ),
+            //Pestañas (TapBar)
+            //Contenido de pestañas (TabBarView)
+            //Carrito (Cart)
+          ],
+        ));
+
   }
 }
