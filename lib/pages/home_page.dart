4b16c954dcb3fb5b85076802cace2858ef27b58b
VecinoLiliana <vecinocollililianamarisol@gmail.com>
2025-02-15 23:30:07 -0600

    Starting TabBarView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,9 @@
+import 'package:donut_app_2c_vecino/tabs/burger_tab.dart';
+import 'package:donut_app_2c_vecino/tabs/donnut_tab.dart';
+import 'package:donut_app_2c_vecino/tabs/pancakes_tab.dart';
+import 'package:donut_app_2c_vecino/tabs/pizza_tab.dart';
+import 'package:donut_app_2c_vecino/tabs/smoothie_tab.dart';
+import 'package:donut_app_2c_vecino/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,44 +14,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State <HomePage> {
+  List<Widget> myTabs = [
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    const MyTab(iconPath: 'lib/icons/pizza.png')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          leading: Icon(Icons.menu, color: Colors.grey[800]),
-          //Ícono derecho
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Icon(Icons.person),
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            //Texto principal
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-              child: Row(
-                children: [
-                  Text("I want to ", style: TextStyle(fontSize: 32)),
-                  Text("Eat",
-                      style: TextStyle(
-                          //Tamaño de letra
-                          fontSize: 32,
-                          //Negritas
-                          fontWeight: FontWeight.bold,
-                          //Subrayado
-                          decoration: TextDecoration.underline))
-                ],
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            leading: Icon(Icons.menu, color: Colors.grey[800]),
+            //Ícono derecho
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Icon(Icons.person),
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              //Texto principal
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+                child: Row(
+                  children: [
+                    Text("I want to ", style: TextStyle(fontSize: 32)),
+                    Text("Eat",
+                        style: TextStyle(
+                            //Tamaño de letra
+                            fontSize: 32,
+                            //Negritas
+                            fontWeight: FontWeight.bold,
+                            //Subrayado
+                            decoration: TextDecoration.underline))
+                  ],
+                ),
               ),
-            ),
-            //Pestañas (TapBar)
-            //Contenido de pestañas (TabBarView)
-            //Carrito (Cart)
-          ],
-        ));
 
+              //Pestañas (TapBar)
+              TabBar(tabs: myTabs),
+              //Contenido de pestañas (TabBarView)
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    DonnutTab(),
+                    BurgerTab(),
+                    SmoothieTab(),
+                    PancakesTab(),
+                    PizzaTab()
+                  ],
+                ),
+              )
+              //Carrito (Cart)
+            ],
+          )),
+    ); 
   }
 }
---
+++ lib/tabs/donnut_tab.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class DonnutTab extends StatelessWidget {
+  const DonnutTab({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('DonnutTab'));
+  }
+}
